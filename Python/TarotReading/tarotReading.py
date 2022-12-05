ded8669ee634afe880cb444523937e911f1bb7ab
Daniel Smith <dsmith75102681@gmail.com>
2022-12-04 19:57:44 -0600

    csv parsed

---
+++ Python/TarotReading/tarotReading.py
@@ -0,0 +1,7 @@
+from PandasToList import PandasToList
+import random
+
+p = PandasToList()
+list_tarot_card = p.tarot_deck("C:/Users/dsmit/Documents/VSCode/Python/TarotReading/lessoncards.csv")
+
+print(list_tarot_card)
